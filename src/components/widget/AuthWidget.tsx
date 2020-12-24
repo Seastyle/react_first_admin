c76f01617e7f45f2a822fdf9eedb9cd61d73505e
seastyle <seastyle1777@gmail.com>
2020-12-25 00:05:33 +0800

    ✨feat: 权限页面调整及学习

--- src/components/widget/AuthWidget.tsx
+++ src/components/widget/AuthWidget.tsx
@@ -1,19 +1,16 @@
-/**
- * Created by 叶子 on 2017/7/31.
- */
 import { Component } from 'react';
 import { connectAlita } from 'redux-alita';
 
 type AuthWidgetProps = {
-    auth: any;
-    children: (param: any) => React.ReactElement;
+  auth: any;
+  children: (param: any) => React.ReactElement;
 };
 
 class AuthWidget extends Component<AuthWidgetProps> {
-    render() {
-        const { auth = {} } = this.props;
-        return this.props.children(auth.data || {});
-    }
+  render() {
+    const { auth = {} } = this.props;
+    return this.props.children(auth.data || {});
+  }
 }
 
 export default connectAlita(['auth'])(AuthWidget);
